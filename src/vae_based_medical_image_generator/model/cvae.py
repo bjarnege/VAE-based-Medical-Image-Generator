5293bdc6e89227f72d379677ee5fb48267bd26d0
Bjarne <bjarne.ge@gmx.de>
2023-07-01 18:30:49 +0200

    Initial cvae architecture + training-loop for cvae

--- src/vae_based_medical_image_generator/model/cvae.py
+++ src/vae_based_medical_image_generator/model/cvae.py
@@ -2,10 +2,11 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-class VariationalAutoencoder(nn.Module):
 
-    def __init__(self, image_channels: int, latent_dimension: int, device: torch.device):
-        super(VariationalAutoencoder, self).__init__()
+class ConditionalVariationalAutoencoder(nn.Module):
+
+    def __init__(self, image_channels: int, n_labels: int, latent_dimension: int, device: torch.device):
+        super(ConditionalVariationalAutoencoder, self).__init__()
         self.device = device
 
         self.encoder = nn.Sequential(
@@ -20,11 +21,11 @@ class VariationalAutoencoder(nn.Module):
 
         hidden_dimension = self.get_encoder_hidden_dimension(image_channels)
 
-        self.mean_fc = nn.Linear(hidden_dimension, latent_dimension)
-        self.log_variance_fc = nn.Linear(hidden_dimension, latent_dimension)
+        self.mean_fc = nn.Linear(hidden_dimension + n_labels, latent_dimension)
+        self.log_variance_fc = nn.Linear(hidden_dimension + n_labels, latent_dimension)
 
         self.decoder = nn.Sequential(
-            nn.Linear(latent_dimension, hidden_dimension),
+            nn.Linear(latent_dimension + n_labels, hidden_dimension),
             torch.nn.Unflatten(1, torch.Size([64, int(np.sqrt(hidden_dimension // 64)),
                                               int(np.sqrt(hidden_dimension // 64))])),
             nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3, stride=1),
@@ -43,11 +44,14 @@ class VariationalAutoencoder(nn.Module):
         epsilon = torch.randn_like(standard_deviation).to(self.device)
         return mean + standard_deviation * epsilon
 
-    def forward(self, x):
+    def forward(self, x, c):
         hidden = self.encoder(x)
-        mean, log_variance = self.mean_fc(hidden), self.log_variance_fc(hidden)
+        hidden_with_labels = torch.cat([hidden, c], 1)
+
+        mean, log_variance = self.mean_fc(hidden_with_labels), self.log_variance_fc(hidden_with_labels)
         sampled_latent_variable = self.reparameterization(mean, log_variance)
-        return self.decoder(sampled_latent_variable), mean, log_variance
+        sampled_latent_variable_with_labels = torch.cat([sampled_latent_variable, c], 1)
+        return self.decoder(sampled_latent_variable_with_labels), mean, log_variance
 
 
 def vae_loss(x, x_hat, mean, log_variance):
